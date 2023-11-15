9f4a20d7e3358ff0f074e2d714bf0b50443c4237
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-15 21:44:04 +0530

    Cleaned Codebase

--- app/(routes)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-function Register() {
-  return (
-    <div className="h-screen w-screen flex justify-center items-start">
-      <form action=""></form>
-    </div>
-  );
-}
-
-export default Register;
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import UsersList from './components/users';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
       <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         Noteworthy
       </h2>
-      <UsersList />
     </main>
   );
 }

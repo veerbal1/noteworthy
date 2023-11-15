c43946fa8dfed958e9c9f820d22a26b5d4e4d2c0
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-15 22:53:19 +0530

    Setup basic login functionality

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../ui/login";
+
+function LoginPage() {
+    return <div className="flex w-screen h-screen items-center justify-center">
+        <LoginForm/>
+    </div>
+}
+
+export default LoginPage;
---
+++ scripts/seed.js
@@ -0,0 +1,51 @@
+const { db } = require('@vercel/postgres');
+const bcrypt = require('bcrypt');
+
+async function seedUsers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      );
+    `;
+
+    console.log(`Created "users" table`);
+
+    // Insert data into the "users" table
+    const hashedPassword = await bcrypt.hash('123456', 10);
+    await client.sql`
+    INSERT INTO users (id, name, email, password)
+    VALUES (${"410544b2-4001-4271-9855-fec4b6a6442b"}, ${'Veerbal'}, ${'user@nextmail.com'}, ${hashedPassword})
+    ON CONFLICT (id) DO NOTHING;
+  `;
+
+    console.log(`Seeded users`);
+
+    return {
+      createTable,
+    };
+  } catch (error) {
+    console.error('Error seeding users:', error);
+    throw error;
+  }
+}
+
+async function main() {
+  const client = await db.connect();
+
+  await seedUsers(client);
+
+  await client.end();
+}
+
+main().catch((err) => {
+  console.error(
+    'An error occurred while attempting to seed the database:',
+    err
+  );
+});

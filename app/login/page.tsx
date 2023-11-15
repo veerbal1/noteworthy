922aed23e510c64ae8d924fe61c082b047fddfc0
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-15 23:59:12 +0530

    Removed old login route

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../ui/login";
-
-function LoginPage() {
-    return <div className="flex w-screen h-screen items-center justify-center">
-        <LoginForm/>
-    </div>
-}
-
-export default LoginPage;

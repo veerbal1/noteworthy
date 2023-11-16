ba607dfcb141a6e9d353f8127cf70964a2e27462
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-16 11:02:30 +0530

    Removed dummy logout button from dashboard

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,21 +1,7 @@
 import { signOut } from '@/auth';
 
 function Dashboard() {
-  return (
-    <div>
-      Dashboard
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
-        <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-          <div className="hidden md:block">Sign Out</div>
-        </button>
-      </form>
-    </div>
-  );
+  return <div>Dashboard</div>;
 }
 
 export default Dashboard;

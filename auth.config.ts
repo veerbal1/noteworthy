b9a79e5876f796157c2fa234167c76254ef60da8
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-16 10:31:06 +0530

    Updated middleware for redirection

--- auth.config.ts
+++ auth.config.ts
@@ -11,14 +11,18 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      console.log(isLoggedIn);
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+
+      // Redirect unauthenticated users to the sign-in page
+      if (!isLoggedIn && nextUrl.pathname === '/') {
+        return true; // Allow access to the sign-in page if not logged in
       }
+
+      // If the user is logged in and tries to access the sign-in page, redirect them
+      if (isLoggedIn && nextUrl.pathname === '/') {
+        return Response.redirect(new URL('/dashboard', nextUrl)); // Redirect to the dashboard or another appropriate page
+      }
+
+      // For all other cases, allow the user to access the requested URL
       return true;
     },
   },

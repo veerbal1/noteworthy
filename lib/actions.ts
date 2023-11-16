ed2f74d58a6d4d30d8d1a92e587d9649ba51b138
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-16 15:01:27 +0530

    II- Created signup functionality

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,8 @@
 'use server';
 import { signIn, signOut } from '@/auth';
+import { z } from 'zod';
+import bcrypt from 'bcrypt';
+import { sql } from '@vercel/postgres';
 
 export async function authenticate(
   prevState: string | undefined,
@@ -12,6 +15,45 @@ export async function authenticate(
       return 'CredentialsSignin';
     }
     throw error;
+  }
+}
+
+const signUpSchema = z.object({
+  name: z.string().min(1, 'Name is required'),
+  email: z.string().email('Invalid Email'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+});
+
+export async function signUpAction(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    const formDataObject = Object.fromEntries(formData);
+    // Validate the form data using the Zod schema
+    const validatedData = signUpSchema.parse(formDataObject);
+    console.log('Validated data', validatedData);
+    const { rowCount } =
+      await sql`SELECT email FROM users WHERE email = ${validatedData.email}`;
+    console.log('Row Count', rowCount);
+    if (rowCount) return 'User already exists';
+    const hashedPassword = await bcrypt.hash(validatedData.password, 10);
+    const { rows } = await sql`
+      INSERT INTO users(name, email, password) VALUES(${validatedData.name}, ${validatedData.email}, ${hashedPassword})
+    `;
+    console.log(rows);
+    return 'User Submitted Successfully';
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      // Log or return the validation error messages
+      console.error(error.errors);
+      return error.errors.map((err) => err.message).join(', ');
+    }
+
+    if ((error as Error).message.includes('CredentialsSignup')) {
+      return 'CredentialsSignup';
+    }
+    return JSON.stringify(error);
   }
 }
 

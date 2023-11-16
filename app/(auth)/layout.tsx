f7d45f56d03b4748ae6ae2542feab5fa5a28359e
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-16 10:45:37 +0530

    Created separte logo component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,23 +1,11 @@
+import Logo from '../_components/logo';
+
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container relative h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-zinc-900" />
-        <div className="relative z-20 flex items-center text-lg font-medium">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
-          Noteworthy
-        </div>
+        <Logo />
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
             <p className="text-lg">
@@ -27,6 +15,7 @@ function Layout({ children }: { children: React.ReactNode }) {
             </p>
             <footer className="text-sm">Anonymous</footer>
           </blockquote>
+          Logo
         </div>
       </div>
       {children}

2cd20bb657e717c56152d5a950e4a89a8afc958c
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-16 16:52:30 +0530

    Removed unused file

--- utils/index.tsx
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { sql } from '@vercel/postgres';
-
-export const getUsers = async () => {
-  const { rows } = await sql`SELECT * from users`;
-  console.log('----Rows-----', rows);
-  return rows;
-};

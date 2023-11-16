0798a1b46b1fb0dae85bea9428bf9d4cd04ae6d4
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-16 10:31:44 +0530

    Moved dashboard to a (group) route

---
+++ app/(pages)/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import { signOut } from '@/auth';
+
+function Dashboard() {
+  return (
+    <div>
+      Dashboard
+      <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+      >
+        <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <div className="hidden md:block">Sign Out</div>
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Dashboard;

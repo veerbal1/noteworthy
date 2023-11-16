fd987b0892153fc50a04d8bf5552766a1008fe2e
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-16 16:53:50 +0530

    Optimized notes fetching database query

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { auth } from '@/auth';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 
@@ -10,11 +11,14 @@ export const getUsers = async () => {
 };
 
 export const getNotes = async (email: string | null | undefined) => {
+  const session = await auth();
+  const { user } = session || { user: null };
   if (!email) return [];
-  const { rows: userRows, rowCount: userRowCount } =
-    await sql`SELECT * FROM users WHERE email = ${email}`;
-  if (userRowCount === 0) return [];
-  const user = userRows[0];
-  const { rows } = await sql`SELECT * FROM notes WHERE userId = ${user.id}`;
+  const { rows } = await sql`
+    SELECT notes.id, notes.title, notes.description 
+    FROM notes 
+    INNER JOIN users ON notes.userId = users.id
+    WHERE users.id = ${user?.id};
+    `;
   return rows;
 };

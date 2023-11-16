97cf065e91cef0b0c1a00f9c09a0efd1d7ceaa5e
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-16 17:35:41 +0530

    Added scrolling to cards window

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,13 +3,13 @@ import Sidebar from './_components/sidebar';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full h-4">
+    <div className="w-full h-screen overflow-hidden">
       <Header />
       <div className="flex w-full h-screen gap-14 pt-4">
         <div className="left w-56 ">
           <Sidebar />
         </div>
-        <div className="right w-full h-full relative pr-36 md:pr-48">{children}</div>
+        <div className="right w-full max-h-screen relative pr-36 md:pr-48 overflow-scroll">{children}</div>
       </div>
     </div>
   );

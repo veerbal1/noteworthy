36c216aa1d816e8be7ad3efbab38713ac035c389
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-19 19:45:15 +0530

    Removed secondary sidebar

--- app/(pages)/notes/layout.tsx
+++ app/(pages)/notes/layout.tsx
@@ -1,12 +1,7 @@
-import SecondarySidebar from '../_components/secondary-sidebar';
-
 function NotesLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex overflow-hidden h-full">
       <div className="flex-1 overflow-scroll">{children}</div>
-      {/* <div className="w-56 sticky top-0">
-        <SecondarySidebar />
-      </div> */}
     </div>
   );
 }

300024ac69c26ab249764d71ef18d3f58b5cba21
Veerbal Singh <Veerbalsingh1@gmail.com>
2023-11-22 19:05:53 +0530

    Removed reminder

--- app/(pages)/_components/sidebar.tsx
+++ app/(pages)/_components/sidebar.tsx
@@ -4,7 +4,6 @@ function Sidebar() {
   return (
     <div className="w-full h-full flex flex-col">
       <SidebarItem title="Notes" link="/notes" />
-      <SidebarItem title="Reminders" link="/notes/reminders" />
     </div>
   );
 }
